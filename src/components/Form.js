d3df17c0b035379a1dcc8c147b70f9d28b47f3e9
Steven Bister <stevebister@gmail.com>
2019-10-17 22:14:28 +0100

    renamed search as form and added submit function

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class Form extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({ value: e.target.value }) // Set property to value of input
+  }
+
+  render () {
+    return (
+      // onChange function is written in parent and set a prop here
+      <div>
+        <form onSubmit={this.props.onSubmit}>
+          <label>
+            Search city:
+            <input type='text' name='search' onChange={this.handleChange} value={this.state.value} />
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    )
+  }
+}
+
+// eslint-disable-next-line react/no-typos
+Form.propTypes = {
+  search: PropTypes.string
+}

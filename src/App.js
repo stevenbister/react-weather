9d42c17601d1f88103e6a45236de78dc610b46b2
Steven Bister <stevebister@gmail.com>
2019-10-17 20:29:31 +0100

    moved components into parent folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 import { Weather } from './components/Weather'
+import { Search } from './components/Search'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      search: undefined
+    }
+
+    // For input function
+    this.onInput = this.onInput.bind(this)
+  }
+
+  // Function to set state when input is detected
+  onInput (search) {
+    this.setState({ search })
+  }
+
   render () {
     return (
-      <Weather />
+      <div>
+        <Search change={this.onInput} />
+        <Weather />
+      </div>
     )
   }
 }

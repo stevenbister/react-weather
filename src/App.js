dd65465f8cf8c2d4e464b8b4a8b735841e328a92
Steven Bister <s.bister@bham.ac.uk>
2019-10-17 12:59:24 +0100

    created new component and linked it to app

--- src/App.js
+++ src/App.js
@@ -1,72 +1,12 @@
 import React from 'react'
 
-import API from './utils/API'
-
-import OPENWEATHERAPIKEY from './config'
+import { Weather } from './components/weather/weather'
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-      city: undefined,
-      weather: undefined,
-      temp: undefined
-    }
-  }
-
-  apiCall() {
-    const CITY = 'Birmingham'
-    const COUNTRY = 'uk'
-    const UNITS = 'metric'
-    API.get(`?q=${CITY},${COUNTRY}&units=${UNITS}&APPID=${OPENWEATHERAPIKEY}`)
-      .then (response => response.data)
-      .then ((data) => {
-        this.setState({
-          isLoaded: true,
-          items: data,
-          city: data.name,
-          weather: data.weather[0].description,
-          temp: data.main.temp
-        })
-      },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        }
-      )
-  }
-
-  componentDidMount () {
-    this.apiCall()
-  }
-
   render () {
-    const { error, isLoaded, city, weather, temp } = this.state
-    if (error) {
-      return <div>Error: {error.message}</div>
-    } 
-    else if (!isLoaded) {
-      return <div>Loading...</div>
-    } 
-    else {
-      // TODO: Seperate these into components
-      return (
-        <div>
-          <h1>{city}</h1>
-          <h2>{weather}</h2>
-          <h2>{temp}&deg;C</h2>
-        </div>
-      )
-    }
+    return (
+      <Weather />
+    )
   }
 }
 

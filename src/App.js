0f5d4954d5836f96cc90345da8d3961e02c35969
Steven Bister <s.bister@bham.ac.uk>
2019-10-18 12:43:41 +0100

    moved api function into weather component so error handling wont effect whole app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import React from 'react'
-
-import API from './utils/API'
-
-import OPENWEATHERAPIKEY from './config'
-
-import { Weather } from './components/Weather'
 import { Form } from './components/Form'
+import { Weather } from './components/Weather'
+
 class App extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-      city: undefined,
-      weather: undefined,
-      temp: undefined,
       search: 'Birmingham'
     }
 
     this.search = this.search.bind(this) // Bind search function
-  }
-
-  apiCall = async () => {
-    const CITY = this.state.search
-    const COUNTRY = 'uk'
-    const UNITS = 'metric'
-    await API.get(`?q=${CITY},${COUNTRY}&units=${UNITS}&APPID=${OPENWEATHERAPIKEY}`)
-      .then(response => response.data)
-      .then(
-        data => {
-          this.setState({
-            isLoaded: true,
-            items: data,
-            city: data.name,
-            weather: data.weather[0].description,
-            temp: data.main.temp
-          })
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        }
-      )
   }
 
   // Set search state to form's name='search' input
@@ -57,39 +19,13 @@ class App extends React.Component {
     this.setState({ search: e.target.elements.search.value })
   }
 
-  // Make api call again if new state doesn't match previous state 
-  componentDidUpdate(prevState) {
-    if (this.state.search !== prevState.search) {
-      this.apiCall()
-    }
-  }
-
-  // Call api once component is mounted
-  componentDidMount () {
-    this.apiCall()
-  }
-
   render () {
-    const { error, isLoaded, city, weather, temp } = this.state
-
-    // if (error) {
-    //   return <div>Error: {error.message}</div>
-    // } else if (!isLoaded) {
-    //   return <div>Loading...</div>
-    // } else {
-      // TODO: Seperate these into components
-      // TODO: Sort error messages
-      return (
-        <div>
-          <Form onSubmit={this.search} />
-          { !isLoaded ? 'Loading...' : null }
-          <h1>{city}</h1>
-          <h2>{weather}</h2>
-          <h2>{temp}&deg;C</h2>
-          { error ? error.message : null } 
-        </div>
-      )
-    // }
+    return (
+      <div>
+        <Form onSubmit={this.search} />
+        <Weather searchTerm={this.state.search} />
+      </div>
+    )
   }
 }
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,79 @@
 import React from 'react'
+import API from '../utils/API'
+import OPENWEATHERAPIKEY from '../config'
 
 export class Weather extends React.Component {
-  // constructor (props) {
-  //   super(props)
-  // }
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      city: undefined,
+      weather: undefined,
+      temp: undefined,
+    }
+  }
+
+  apiCall = async () => {
+    const CITY = this.props.searchTerm
+    // const CITY = 'Birmingham'
+    const COUNTRY = 'uk'
+    const UNITS = 'metric'
+    await API.get(`?q=${CITY},${COUNTRY}&units=${UNITS}&APPID=${OPENWEATHERAPIKEY}`)
+      .then(response => response.data)
+      .then(
+        data => {
+          this.setState({
+            isLoaded: true,
+            items: data,
+            city: data.name,
+            weather: data.weather[0].description,
+            temp: data.main.temp
+          })
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  // Call api once component is mounted
+  componentDidMount () {
+    this.apiCall()
+  }
+
+  // Make api call again if new state doesn't match previous state 
+  componentDidUpdate(prevProps) {
+    if (this.props.searchTerm !== prevProps.searchTerm) {
+      this.apiCall()
+    }
+  }
 
   render () {
-    return <p>Weather</p>
+    const { error, isLoaded, city, weather, temp } = this.state
+
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      // TODO: Seperate these into components
+      // TODO: Sort error messages
+      return (
+        <div>
+          <h1>{city}</h1>
+          <h2>{weather}</h2>
+          <h2>{temp}&deg;C</h2>
+        </div>
+      )
+    }
   }
 }

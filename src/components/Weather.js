feee5b40074203772d2140f0902d1cb82de9ceb0
Steven Bister <stevebister@gmail.com>
2019-10-21 19:12:29 +0100

    added path to weather icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 
 export class Weather extends React.Component {
   render () {
-    const ICON = this.props.icon
-    //TODO: upload own images to replace this proof of concept
-    // List of icon codes here: https://openweathermap.org/weather-conditions
-    const IMG = `https://openweathermap.org/img/wn/${ICON}@2x.png` 
+    const icon = this.props.icon
+    // As we need to dynamically reference icon we place it into the public folder
+    //  https://create-react-app.dev/docs/using-the-public-folder
+    const imgPath = `${process.env.PUBLIC_URL}/img/${icon}.svg`
     return (
       <div>
         <p>{this.props.weather}</p>
-        <img src={IMG} alt="" />
+        <img src={imgPath} alt="" />
       </div>
     )
   }
